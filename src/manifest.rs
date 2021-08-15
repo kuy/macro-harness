46cc43950b2f030014a6dd14a690b83eb75b70c2
Yuki Kodama <endflow.net@gmail.com>
2021-08-16 02:10:58 +0900

    Make manifest generation testable

--- src/manifest.rs
+++ src/manifest.rs
@@ -14,27 +14,49 @@ pub fn prepare_manifest_file(
     // Parepare temporary directory
     let dir = path::create_temp_dir();
     let temp_manifest_path = dir.join("Cargo.toml");
+    let manifest_dir = template_path.as_ref().parent().expect("should have parent");
 
-    // Deserialize template manifest file
-    let content = fs::read_to_string(&template_path)?;
-    let mut manifest = cargo_toml::Manifest::from_slice(content.as_bytes()).unwrap_or_else(|err| {
+    // Generate manifest base on template
+    let template = fs::read_to_string(&template_path).unwrap_or_else(|err| {
         panic!(
-            "Failed to load manifest: {:?}\n  Error: {:?}",
+            "Failed to read manifest: {:?}\n  Error: {:?}",
             template_path.as_ref(),
             err
         );
     });
+    let content = generate_manifest(&template, source_path, |rel_path| {
+        path::canonicalize(&manifest_dir, &rel_path)
+    });
+    let mut temp_manifest = File::create(&temp_manifest_path).unwrap_or_else(|err| {
+        panic!(
+            "Failed to create manifest file: {:?}\n  Error: {:?}",
+            temp_manifest_path, err
+        );
+    });
+    temp_manifest.write_all(content.as_bytes())?;
+    temp_manifest.flush()?;
+
+    Ok((temp_manifest_path, dir))
+}
+
+fn generate_manifest<P, F>(template: &str, source_path: P, rel_to_abs: F) -> String
+where
+    P: AsRef<Path>,
+    F: Fn(&str) -> PathBuf,
+{
+    let mut manifest =
+        cargo_toml::Manifest::from_slice(template.as_bytes()).unwrap_or_else(|err| {
+            panic!("Failed to parse manifest\n  Error: {:?}", err);
+        });
 
     // Apply modifications: deps, lib
-    let manifest_path = template_path.as_ref().to_path_buf();
     manifest.dependencies = manifest
         .dependencies
         .into_iter()
         .map(|(crate_name, dep)| match dep.clone() {
             Dependency::Detailed(detail) => {
                 if let Some(rel_path) = detail.clone().path {
-                    let manifest_dir = manifest_path.parent().expect("should be have parent");
-                    let crate_dir = path::canonicalize(&manifest_dir, &rel_path);
+                    let crate_dir = rel_to_abs(&rel_path);
                     let detail = DependencyDetail {
                         path: Some(String::from(crate_dir.to_str().unwrap())),
                         ..detail
@@ -55,18 +77,34 @@ pub fn prepare_manifest_file(
         ..Default::default()
     });
 
-    // Write out manifest
-    let content = toml::to_string(&manifest).unwrap_or_else(|err| {
+    toml::to_string(&manifest).unwrap_or_else(|err| {
         panic!("Failed to serialize manifest\n  Error: {:?}", err);
-    });
-    let mut temp_manifest = File::create(&temp_manifest_path).unwrap_or_else(|err| {
-        panic!(
-            "Failed to create manifest file: {:?}\n  Error: {:?}",
-            temp_manifest_path, err
-        );
-    });
-    temp_manifest.write_all(content.as_bytes())?;
-    temp_manifest.flush()?;
+    })
+}
 
-    Ok((temp_manifest_path, dir))
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::diff::print;
+    use std::str::FromStr;
+
+    #[test]
+    fn test_generate_manifest() {
+        let source_path =
+            PathBuf::from_str("/home/macro-harness/projects/awesome/tests/test_macro.rs").unwrap();
+        print(
+            include_str!("../fixtures/Cargo.expected.toml"),
+            generate_manifest(
+                include_str!("../fixtures/Cargo.template.toml"),
+                source_path,
+                |rel_path| {
+                    let rel = String::from(rel_path);
+                    let name = rel.split('/').last().unwrap();
+                    PathBuf::from_str(format!("/home/macro-harness/projects/{}", name).as_str())
+                        .unwrap()
+                },
+            )
+            .as_str(),
+        );
+    }
 }
